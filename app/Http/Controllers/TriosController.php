3000a71926860f99d586c52db7668025ae8a7b39
Farrukh Choriyev <choriyevfarrux@mail.ru>
2019-07-22 17:12:45 +0500

    second Commit

---
+++ app/Http/Controllers/TriosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Trio;
+use Illuminate\Http\Request;
+
+class TriosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Trio  $trio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trio $trio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Trio  $trio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trio $trio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Trio  $trio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trio $trio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Trio  $trio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trio $trio)
+    {
+        //
+    }
+}
